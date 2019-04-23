a351673dc4a3d7ade324ed03dc153879abac84a4
VT61661 <TambeVikramC@JohnDeere.com>
2019-04-23 14:42:53 -0500

    fixed issues

--- src/QuizQuestionButton.js
+++ src/QuizQuestionButton.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react'
 class QuizQuestionButton extends Component{
   render(){
     return(
-      <li><button> {this.props.button_text}</button></li>
+      <li><button>{this.props.button_text}</button></li>
     )
   }
 }
